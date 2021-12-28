920c2b9c4c5d5834ab451affa6b770d514bf7d56
jessicaSoares <jessicassoarez@gmail.com>
2021-12-28 01:05:52 -0300

    remove

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+import {  BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-function App() {
+import AddTutorial from "./components/add-tutorial.component";
+import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorials-list.component";
+
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/tutorials" className="navbar-brand">
+            jessDev
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className = "nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link> 
+            </li>
+            </div> 
+        </nav>
+
+        <div className="container mt-3">
+          <Routes> 
+            <Route exact path={"/tutorials"} element={<TutorialsList />} />
+            <Route exact path="/add" element = {<AddTutorial />} />
+            <Route path="/customer/:id" element={<Tutorial />} />
+          </Routes>
+          </div>
+        </div>
+        </ Router>
+
+    );
+  }
 }
 
 export default App;
